9d8132f7f7ede773019b5986a8a5ee6032622fa4
David Babbitt <davidbabbitt@pingidentity.com>
2018-02-21 01:18:56 -0600

    convert the SCIM style list response

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,4 +1,13 @@
 import DS from 'ember-data';
+import _ from 'lodash';
 
 export default DS.JSONSerializer.extend({
+  normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
+    // deal with SCIM style list response
+    if (_.isObject(payload) && _.isArray(payload.Resources)) {
+      payload = payload.Resources;
+    }
+    const result = this._super(store, primaryModelClass, payload, id, requestType);
+    return result;
+  }
 });
