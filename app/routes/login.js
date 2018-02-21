9069b931fae2b434c435640ce86c801fdf238467
David Babbitt <davidbabbitt@pingidentity.com>
2018-02-21 02:39:16 -0600

    pass redirect uri

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Route.extend({
+
+export default Route.extend(UnauthenticatedRouteMixin, {
   beforeModel() {
-    window.location.href = "https://pf.babbtx.work:9031/as/authorization.oauth2?client_id=fakebank-ui&response_type=token+id_token";
+    const url = "https://pf.babbtx.work:9031/as/authorization.oauth2"
+      + "?client_id=fakebank-ui"
+      + `&redirect_uri=${window.location.origin}/oauth-callback`
+      + "&response_type=token+id_token";
+    window.location.replace(url);
   }
 });
