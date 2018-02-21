5a5f68fc2db1d8240b7447cfe7fda77a0954a6fa
David Babbitt <davidbabbitt@pingidentity.com>
2018-02-21 02:40:23 -0600

    pass bearer token to API

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,12 +1,12 @@
 import { pluralize } from 'ember-inflector';
 import { underscore } from '@ember/string';
 import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-
-export default DS.RESTAdapter.extend({
+export default DS.RESTAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:oauth-bearer',
 
   pathForType(type) {
     return pluralize(underscore(type));
   }
-
 });
