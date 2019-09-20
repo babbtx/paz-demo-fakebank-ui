bc000ec79cd0fad7e676dada4a73610e94535573
David Babbitt <davidbabbitt@pingidentity.com>
2019-09-20 00:56:22 -0500

    capture API history in an array

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,11 +1,13 @@
 import DS from 'ember-data';
 import { pluralize } from 'ember-inflector';
 import { isPresent } from '@ember/utils';
+import { inject as service } from '@ember/service';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from '../config/environment';
 
 export default DS.RESTAdapter.extend(DataAdapterMixin, {
   host: ENV.backend,
+  history: service('api-history'),
 
   authorize(xhr) {
     let { access_token } = this.get('session.data.authenticated');
@@ -16,5 +18,10 @@ export default DS.RESTAdapter.extend(DataAdapterMixin, {
 
   pathForType(type) {
     return pluralize(type);
+  },
+
+  handleResponse(status, headers, payload, requestData) {
+    this.get('history').add(requestData.method, requestData.url, status, payload);
+    return this._super(...arguments);
   }
 });
