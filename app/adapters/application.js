5393dff50d2f40b687d888fcbf09c4a153e99c64
David Babbitt <davidbabbitt@pingidentity.com>
2019-09-19 21:25:42 -0500

    updating to not use authorizer

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,11 +1,18 @@
-import { pluralize } from 'ember-inflector';
 import DS from 'ember-data';
+import { pluralize } from 'ember-inflector';
+import { isPresent } from '@ember/utils';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from '../config/environment';
 
 export default DS.RESTAdapter.extend(DataAdapterMixin, {
   host: ENV.backend,
-  authorizer: 'authorizer:oauth-bearer',
+
+  authorize(xhr) {
+    let { access_token } = this.get('session.data.authenticated');
+    if (isPresent(access_token)) {
+      xhr.setRequestHeader('Authorization', `Bearer ${access_token}`);
+    }
+  },
 
   pathForType(type) {
     return pluralize(type);
--- app/routes/oauth-callback.js
+++ app/routes/oauth-callback.js
@@ -2,5 +2,5 @@ import Route from '@ember/routing/route';
 import OAuth2ImplicitGrantCallbackMixin from 'ember-simple-auth/mixins/oauth2-implicit-grant-callback-route-mixin';
 
 export default Route.extend(OAuth2ImplicitGrantCallbackMixin, {
-  authenticator: 'authenticator:pf',
+  authenticator: 'authenticator:oauth2-implicit',
 });
