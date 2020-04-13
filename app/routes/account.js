60f059dbecb6e3da8ec138b614e9ffde37384cd9
David Babbitt <davidbabbitt@pingidentity.com>
2020-04-13 18:23:38 -0500

    added error pages

--- app/routes/account.js
+++ app/routes/account.js
@@ -3,9 +3,11 @@ import RSVP  from 'rsvp';
 
 export default Route.extend({
   model(params) {
+    this.set('accountId', params.account_id);
+
     return RSVP.hashSettled({
-      account: this.store.findRecord('account', params.account_id),
-      balance: this.store.findRecord('balance', params.account_id)
+      account: this.store.findRecord('account', params.account_id, {reload: true}),
+      balance: this.store.findRecord('balance', params.account_id, {reload: true})
     }).then(function(hash) {
       if (hash.account.value) {
         return hash;
@@ -17,9 +19,21 @@ export default Route.extend({
   },
 
   setupController(controller, hash) {
+    controller.set('accountId', this.get('accountId'));
     controller.set('account', hash.account.value);
     if (hash.balance.value) {
       controller.set('balance', hash.balance.value);
+    }
+  },
+
+  actions: {
+    refreshAccount() {
+      this.refresh();
+    },
+    error(error) {
+      // HACK passing the accountId to the error route via the error object
+      error.accountId = this.get('accountId');
+      return true;
     }
   }
 });
