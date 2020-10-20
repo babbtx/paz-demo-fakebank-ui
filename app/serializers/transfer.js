b64a33b0ecbe81ed8031768e198095c110bc3713
David Babbitt <davidbabbitt@pingidentity.com>
2020-10-20 01:16:24 -0500

    lint

--- app/serializers/transfer.js
+++ app/serializers/transfer.js
@@ -2,11 +2,11 @@ import DS from 'ember-data';
 import { underscore } from '@ember/string';
 
 export default DS.JSONAPISerializer.extend({
-  keyForAttribute(key, method) {
+  keyForAttribute(key) {
     return underscore(key);
   },
 
-  extractErrors(store, typeClass, payload, id) {
+  extractErrors(store, typeClass, payload) {
     /*
     FROM JSON-API format:
 
