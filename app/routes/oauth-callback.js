2ad038b446ef07d91d4ba80698416a71baf41298
David Babbitt <davidbabbitt@pingidentity.com>
2021-02-08 23:30:14 -0600

    fix callback route so that it doesn't flash the error while authentication is processing

--- app/routes/oauth-callback.js
+++ app/routes/oauth-callback.js
@@ -1,27 +1,40 @@
 import Route from '@ember/routing/route';
+import Error from '@ember/error';
 import OAuth2ImplicitGrantCallbackMixin from 'ember-simple-auth/mixins/oauth2-implicit-grant-callback-route-mixin';
 
 export default Route.extend(OAuth2ImplicitGrantCallbackMixin, {
   authenticator: 'authenticator:oauth2-implicit',
-  activate() {
-    // overrides the mixin to not get the parsed hash from the URL
-    // instead get it from local storage, which is set by oauth-callback.html
-    const authenticator = this.get('authenticator');
+
+  model() {
+    // the mixin expects to get the parsed hash from the URL in activate()
+    // but we've changed app behavior to not redirect into Ember and instead
+    // redirect to a static html page that saves the parsed hash to local storage.
+
+    // load that here and complete the authenticatication with that.
     const encodedHash = window.localStorage.getItem('fakebank-ui-oauth-hash');
     const parsedHash = encodedHash ? JSON.parse(encodedHash) : {error: 'Hmm, something went wrong.'};
+
+    // clear what was there
     window.localStorage.setItem('fakebank-ui-oauth-hash', '');
+
     // prefer the error description
+    // not sure whether that's a standard thing or a PingOne thing
     if (parsedHash.error && parsedHash.error_description) {
       parsedHash.error = parsedHash.error_description;
     }
-    this.get('session').authenticate(authenticator, parsedHash).catch((err) => {
-      this.set('error', err);
-      this.set('controller.error', err);
+
+    // the advantage of doing all of this in model() is that errors can propagate
+    // like normal errors in Ember, but we need to wrap it in an Ember.Error
+    const authenticator = this.get('authenticator');
+    return this.get('session').authenticate(authenticator, parsedHash).catch((err) => {
+      throw new Error(err);
     });
   },
+
+  activate() {
+  },
+
   didTransition() {
-    if (!this.get('error')) {
-      this.transitionTo(this.get('routeAfterAuthentication'));
-    }
+    this.transitionTo(this.get('routeAfterAuthentication'));
   }
 });
